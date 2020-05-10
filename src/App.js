a9cc39df894fae9ea944776d7347a5bb38ce8972
quoc27690 <quoc27690@gmail.com>
2020-05-10 21:22:14 +0700

    05-Create-React-App

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>Go to market</li>
+        <li>Buy food</li>
+        <li>Make dinner</li>
+      </ul>
     </div>
   );
 }
